1cd78e518cf64e1afd60a389fabd23aba5fe0d6b
Md. Sindabad <pavel.sindabad@gmail.com>
2021-09-17 12:29:39 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React, { Fragment } from 'react'
 import Navbar from './components/navbar/Navbar'
-import Home from './components/pages/home/Home'
-import Movies from './components/pages/Movies'
-import TvSeries from './components/pages/TvSeries'
-import Details from './components/pages/Details'
-import { Switch, Route } from 'react-router-dom'
+import Footer from './components/Footer'
+import Body from './components/Body'
 
 const App = () => {
     return (
         <Fragment>
             <Navbar />
-            <Switch>
-                <Route exact path='/'>
-                    <Home />
-                </Route>
-                <Route path='/movies'>
-                    <Movies />
-                </Route>
-                <Route path='/tv-series'>
-                    <TvSeries />
-                </Route>
-                <Route path='/details:id'>
-                    <Details />
-                </Route>
-                
-            </Switch>
+            <Body />
+            <Footer />
         </Fragment>
     )
 }
