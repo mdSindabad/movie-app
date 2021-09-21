b83366a26884ec291893532ceb938508a02c3bed
Md. Sindabad <pavel.sindabad@gmail.com>
2021-09-21 21:46:21 +0600

    minor style adjustment

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -30,7 +30,7 @@ const Home = () => {
                 <div className='pb-16'>
                     <ImageSlider data={data} />
                     <section className='p-5 -mt-2'>
-                        <h1 className='text-gray-300 text-3xl mb-3 md:m-5'>Latest Movies and TvSeries</h1>
+                        <h1 className='text-gray-300 text-xl md:text-2xl mb-3 md:m-5'>Latest Movies and TvSeries</h1>
                         <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4'>
                             {
                                 data.map(media => {
