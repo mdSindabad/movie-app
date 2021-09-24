a724076d97a3b2293ad680238e71a32e7f8c2ae5
Md. Sindabad <pavel.sindabad@gmail.com>
2021-09-24 20:16:40 +0600

    search error added

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import Card from '../Card';
 import Spinner from '../Spinner';
 import Error from '../Error';
+import { BiError } from 'react-icons/bi';
 
 
 const Search = ({ history }) => {
@@ -20,18 +21,24 @@ const Search = ({ history }) => {
             error ?
                 // display error message
                 <Error /> :
-                <div className='pb-16 md:pt-16'>
-                    <section className='p-5 -mt-2'>
-                        <h1 className='text-gray-300 text-xl md:text-2xl mb-3 md:m-5'><span className='text-gray-500'>Results for:</span> {searchParams.split('-').join(' ')}</h1>
-                        <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4'>
-                            {
-                                data.map(media => {
-                                    return media.poster_path && <Card key={media.id} media={media} />
-                                })
-                            }
-                        </div>
-                    </section>
-                </div>
+                data.length === 0 ?
+                    <div className='h-screen flex justify-center items-center' >
+                        <h1 className='px-4 py-2 bg-red-200 text-red-400 inline rounded text-xl flex items-center'>
+                            <BiError className='mr-1 text-red-500' />
+                            No results found for : <b className='font-bold text-red-500'>{searchParams.split('-').join(' ')}</b></h1>
+                    </div> :
+                    <div className='pb-16 md:pt-16'>
+                        <section className='p-5 -mt-2'>
+                            <h1 className='text-gray-300 text-xl md:text-2xl mb-3 md:m-5'><span className='text-gray-500'>Results for:</span> {searchParams.split('-').join(' ')}</h1>
+                            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4'>
+                                {
+                                    data.map(media => {
+                                        return media.poster_path && <Card key={media.id} media={media} />
+                                    })
+                                }
+                            </div>
+                        </section>
+                    </div>
     )
 }
 
